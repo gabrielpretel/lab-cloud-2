ac1bdb7e13c8dcca1e91ee822a6b85b32bc0a851
Gabriel Pretel <gabrilinex@gmail.com>
2025-04-23 11:26:12 +0200

    Refactor champion image imports to add correctly in deploy

--- src/common/mock-data/champions-data.ts
+++ src/common/mock-data/champions-data.ts
@@ -1,3 +1,16 @@
+import ahriImg from "@/common/img/ahri.jpg";
+import asheImg from "@/common/img/ashe.jpg";
+import caitlynImg from "@/common/img/caitlyn.jpg";
+import evelynnImg from "@/common/img/evelynn.jpg";
+import gwenImg from "@/common/img/gwen.jpg";
+import jinxImg from "@/common/img/jinx.jpg";
+import kaisaImg from "@/common/img/kaisa.jpg";
+import kindredImg from "@/common/img/kindred.jpg";
+import morganaImg from "@/common/img/morgana.jpg";
+import pantheonImg from "@/common/img/pantheon.jpg";
+import renataImg from "@/common/img/renata.jpg";
+import urgotImg from "@/common/img/urgot.jpg";
+
 export interface ChampionEntity {
   id: number;
   name: string;
@@ -21,7 +34,7 @@ export const CHAMPIONDATA: ChampionEntity[] = [
     id: 1,
     name: "Ahri",
     location: "Ionia",
-    img: "/src/common/img/ahri.jpg",
+    img: ahriImg,
     bgPosition: "67.4897% 14.6444%",
     selected: false,
   },
@@ -29,7 +42,7 @@ export const CHAMPIONDATA: ChampionEntity[] = [
     id: 2,
     name: "Ashe",
     location: "Freljord",
-    img: "/src/common/img/ashe.jpg",
+    img: asheImg,
     bgPosition: "69.4271% 5.44148%",
     selected: false,
   },
@@ -37,7 +50,7 @@ export const CHAMPIONDATA: ChampionEntity[] = [
     id: 3,
     name: "Caitlyn",
     location: "Piltover",
-    img: "/src/common/img/caitlyn.jpg",
+    img: caitlynImg,
     bgPosition: "84.2798% 0.97629%",
     selected: false,
   },
@@ -45,7 +58,7 @@ export const CHAMPIONDATA: ChampionEntity[] = [
     id: 4,
     name: "Evelynn",
     location: "Runeterra",
-    img: "/src/common/img/evelynn.jpg",
+    img: evelynnImg,
     bgPosition: "51.1979% 11.6505%",
     selected: false,
   },
@@ -53,7 +66,7 @@ export const CHAMPIONDATA: ChampionEntity[] = [
     id: 5,
     name: "Gwen",
     location: "Shadow Isles",
-    img: "/src/common/img/gwen.jpg",
+    img: gwenImg,
     bgPosition: "50% 21.1827%",
     selected: false,
   },
@@ -61,7 +74,7 @@ export const CHAMPIONDATA: ChampionEntity[] = [
     id: 6,
     name: "Jinx",
     location: "Zaun",
-    img: "/src/common/img/jinx.jpg",
+    img: jinxImg,
     bgPosition: "74.1564% 11.4365%",
     selected: false,
   },
@@ -69,7 +82,7 @@ export const CHAMPIONDATA: ChampionEntity[] = [
     id: 7,
     name: "Kai'sa",
     location: "The Void",
-    img: "/src/common/img/kaisa.jpg",
+    img: kaisaImg,
     bgPosition: "49.4792% 17.6523%",
     selected: false,
   },
@@ -77,7 +90,7 @@ export const CHAMPIONDATA: ChampionEntity[] = [
     id: 8,
     name: "Kindred",
     location: "Runeterra",
-    img: "/src/common/img/kindred.jpg",
+    img: kindredImg,
     bgPosition: "80.9053% 30.6834%",
     selected: false,
   },
@@ -85,7 +98,7 @@ export const CHAMPIONDATA: ChampionEntity[] = [
     id: 9,
     name: "Morgana",
     location: "Demacia",
-    img: "/src/common/img/morgana.jpg",
+    img: morganaImg,
     bgPosition: "88.7962% 49.2602%",
     selected: false,
   },
@@ -93,7 +106,7 @@ export const CHAMPIONDATA: ChampionEntity[] = [
     id: 10,
     name: "Pantheon",
     location: "Targon",
-    img: "/src/common/img/pantheon.jpg",
+    img: pantheonImg,
     bgPosition: "51.4815% 36.1068%",
     selected: false,
   },
@@ -101,7 +114,7 @@ export const CHAMPIONDATA: ChampionEntity[] = [
     id: 11,
     name: "Renata Glasc",
     location: "Zaun",
-    img: "/src/common/img/renata.jpg",
+    img: renataImg,
     bgPosition: "52.0833% 13.8889%",
     selected: false,
   },
@@ -109,7 +122,7 @@ export const CHAMPIONDATA: ChampionEntity[] = [
     id: 12,
     name: "Urgot",
     location: "Zaun",
-    img: "/src/common/img/urgot.jpg",
+    img: urgotImg,
     bgPosition: "56.4609% 5.5788%",
     selected: false,
   },
